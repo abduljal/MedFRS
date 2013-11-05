1d7c7b227aa6ab9602da395fb16b4d3d0bd39ca8
Anfal Abdul Jaleel <anfalaj@gmail.com>
2013-11-05 10:31:00 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedFRSApp::Application.config.secret_key_base = '815ad441400a3ca476a874017f7895197739d4b27b6be6429d721bca45f2bc6e96bec0a383e83c94258cc585560b2a548fdf26e872f6d10576e384d2a789e8b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MedFRSApp::Application.config.session_store :cookie_store, key: '_MedFRS_app_session'
